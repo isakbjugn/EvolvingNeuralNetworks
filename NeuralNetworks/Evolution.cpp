21134308e150429325af6bab23e9ceaba0ad8759
isakbjugn <42857774+isakbjugn@users.noreply.github.com>
2020-06-21 18:45:01 +0200

    Revert master to 2020-06-09 version. Erratum for previous commit: 06 is correct month

--- NeuralNetworks/Evolution.cpp
+++ NeuralNetworks/Evolution.cpp
@@ -1,14 +1,10 @@
 #include "Evolution.h"
 
-int evolution(int initPopulation, int generations)
+int evolution(int population, int generations)
 {
-	// Correct population to multiple of cores
-	int cores = 4;
-	int population = initPopulation - (initPopulation % cores);
-
 	// Print timestamp
 	cout << "Evolving neural networks with genetic algorithm\n"
-		<< population << " individuals, " << generations << " generations\n"
+		<< population << " individuals, " << generations << " generations\n" 
 		<< currentDateTime() << "\n\n";
 
 	// Declare variables
@@ -17,11 +13,10 @@ int evolution(int initPopulation, int generations)
 	vector<double> fitnesses;
 	fitnesses.reserve(generations);
 	shared_ptr<Network> winner;
-	vector<std::thread> threads(cores);
+	vector<std::thread> threads(population);
 	double currentFitness = 0;
 	double gap = 0.2;
 	vector<int> durations(generations);
-	int groups = population / cores;
 
 	// Initialise start population with random parameters
 	for (int i = 0; i < population; i++)
@@ -39,40 +34,22 @@ int evolution(int initPopulation, int generations)
 		start = std::chrono::high_resolution_clock::now();
 
 		// Print status
-		std::cout << "Generation " << g << "\n";
+		std::cout << "Generation " << g;
 
-		// Single thread
+		/*Multi-thread approach*/
+		// Iterate through current population
 		for (int i = 0; i < population; i++)
-			generation[i]->operator()();
-
-		// Naive multithread
-		/*for (int i = 0; i < population; i++)
 		{
 			shared_ptr<Network> individual = generation[i];
 			threads[i] = std::thread(std::ref(*individual));
 		}
 
 		for (int i = 0; i < population; i++)
-			threads[i].join();*/
+			threads[i].join();
 
-			/*Single-thread approach*/
-			//for (int i = 0; i < population; i++)
-			//	generation[i]->operator()();
-
-		// Informed multithread
-		/*for (int group = 0; group < groups; group++)
-		{
-			std::cout << "Group " << group << "\n";
-
-			for (int core = 0; core < cores; core++)
-			{
-				shared_ptr<Network> individual = generation[core + cores * group];
-				threads[core] = std::thread(std::ref(*individual));
-			}
-
-			for (int core = 0; core < cores; core++)
-				threads[core].join();
-		}*/
+		/*Single-thread approach*/
+		//for (int i = 0; i < population; i++)
+		//	generation[i]->operator()();
 
 		// Print status
 		currentFitness = findFittest(generation)->getKS();
@@ -87,12 +64,9 @@ int evolution(int initPopulation, int generations)
 
 		// End evolution if last generation
 		if (g == (generations - 1))
-		{
-			std::cout << "\t-Evolution complete-\n\n";
 			break;
-		}
 
-		std::cout << "\t-selection commenced-\n\n";
+		std::cout << "\t-selection commenced-\n";
 
 		// Initialise new population
 		generation = selectDescendants(generation, gap);
@@ -102,11 +76,20 @@ int evolution(int initPopulation, int generations)
 			individual->clear();
 	}
 
-	// Record time elapsed for first 100 ms simulation
-	recordRuntime(generation);
-
 	// Select highest performing Network from generation
 	winner = findFittest(generation);
+
+	// Save duration vector
+	/*
+	string fileStr = "durations";
+	fileStr += "-p" + std::to_string(population)
+		+ "-g" + std::to_string(generations)
+		+ "-n" + std::to_string(winner->getNeurons())
+		+ "-d" + std::to_string(winner->getDuration())
+		+ ".dat";
+	const char* filename = fileStr.c_str();
+	saveVector(filename, durations);
+	*/
 
 	// Assess strategy by visualization
 	winner->visualize();
@@ -115,7 +98,10 @@ int evolution(int initPopulation, int generations)
 	std::cout << "\n\nResulting fitness (KS): " << winner->getKS() << "\n";
 
 	// Produce fitness statistics
-	saveVector("fitness-vector.dat", fitnesses);
+	std::cout << "Historic fitness: ";
+	for (const double& k :fitnesses)
+		std::cout << k << ", ";
+	std::cout << "\n";
 
 	return 0;
 }
@@ -168,22 +154,6 @@ shared_ptr<Network> findFittest(vector<shared_ptr<Network>> &generation)
 	return generation[0];
 }
 
-void recordRuntime(const vector<shared_ptr<Network>> &generation)
-{
-	int pop = generation.size();
-	std::vector<int> t100;
-	std::vector<int> f100;
-	t100.reserve(pop);
-	f100.reserve(pop);
-	for (const auto ind : generation)
-	{
-		t100.push_back(ind->getT100());
-		f100.push_back(ind->getF100());
-	}
-	saveVector("time-elapsed-100ms.dat", t100);
-	saveVector("num-firings-100ms.dat", f100);
-}
-
 void testNetworkConstructor()
 {
 	Network Adam;
@@ -212,51 +182,4 @@ void testKappaComparison()
 	Network Datsa = Adam;
 	Datsa();
 	std::cout << "Network kappa: " << Datsa.getKappa() << std::endl;
-}
-
-void testSpeed()
-{
-	int pop = 3;
-	std::vector<std::shared_ptr<Network>> Eden;
-	std::vector<std::thread> threads(pop);
-
-	// Define time variables
-	auto start = std::chrono::high_resolution_clock::now();
-	auto stop = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-
-	// Initialise start population with random parameters
-	for (int seed = 0; seed < pop; seed++)
-		Eden.push_back(make_shared<Network>(seed));
-
-	// Loop which iterates through generations
-	for (int g = 0; g < 1; g++)
-	{
-		// Print status
-		std::cout << "Generation " << g;
-
-		// Start clock
-		start = std::chrono::high_resolution_clock::now();
-
-		/*Multi-thread approach*/
-		// Iterate through current population
-		/*for (int i = 0; i < pop; i++)
-		{
-			shared_ptr<Network> individual = Eden[i];
-			threads[i] = std::thread(std::ref(*individual));
-		}
-
-		for (int i = 0; i < pop; i++)
-			threads[i].join();*/
-
-		/*Single-thread approach*/
-		for (int i = 0; i < pop; i++)
-			Eden[i]->operator()();
-
-		// Stop clock
-		stop = std::chrono::high_resolution_clock::now();
-		duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-		std::cout << "\t" << duration.count() << " ms";
-	}
-	std::cout << "\n\n";
 }
